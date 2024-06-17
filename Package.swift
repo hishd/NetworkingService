e58c96866ba3a73ff373355018188ab215df3d43
HishD <hisharadilshan3@gmail.com>
2024-06-17 02:00:31 +0100

    Changed package name to NetworkingService

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Networking",
+    name: "NetworkingService",
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Networking",
-            targets: ["Networking"]),
+            name: "NetworkingService",
+            targets: ["NetworkingService"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Networking"),
+            name: "NetworkingService"),
         .testTarget(
             name: "NetworkingTests",
-            dependencies: ["Networking"]),
+            dependencies: ["NetworkingService"]),
     ]
 )
